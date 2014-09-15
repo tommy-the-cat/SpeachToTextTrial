7ce0099d3690e6b0cc570ae4adbf4e3765d98c83
Nigel Martin <nigelgmartin@live.ca>
2014-09-15 09:31:03 -0300

    voice propt changed

--- app/src/main/java/mobi/nbcc/voicerecognition/VoiceRecognitionActivity.java
+++ app/src/main/java/mobi/nbcc/voicerecognition/VoiceRecognitionActivity.java
@@ -59,7 +59,7 @@ public class VoiceRecognitionActivity extends Activity {
     {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say a number...");
         startActivityForResult(intent, REQUEST_CODE);
     }
 
